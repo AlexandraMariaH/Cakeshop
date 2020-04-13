fffe84a5d7c617e6f0c36ca5810e77801b93cd09
alexandrahufnagel <alexandra.hufnagel@gmx.de>
2020-04-13 10:11:03 +0200

    added tests and first endpoints

--- src/test/java/com/cakeshop/restservice/CustomerControllerHttpRequestTest.java
+++ src/test/java/com/cakeshop/restservice/CustomerControllerHttpRequestTest.java
@@ -1,25 +1,26 @@
-//package com.cakeshop.restservice;
-//
-//import com.cakeshop.AccessingDataJpaApplication;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.web.client.TestRestTemplate;
-//import org.springframework.boot.web.server.LocalServerPort;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//public class CustomerControllerHttpRequestTest {
-//
-//  //  @LocalServerPort
-//   // private int port;
-//
-//    @Autowired
-//    private TestRestTemplate restTemplate;
-//
-//    @Test
-//    public void customerShouldReturnCustomerInformation() throws Exception {
-//        assertThat(this.restTemplate.getForObject("http://localhost:" + "8081" + "api/customer/customer/allCustomers", String.class)).contains("[{\"id\":2,\"firstName\":\"Jens\",\"lastName\":\"Krupp\",\"emailAdress\":\"Jens.Krupp@gmail.com\"}]");
-//    }
-//}
+package com.cakeshop.restservice;
+
+import com.cakeshop.AccessingDataJpaApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = AccessingDataJpaApplication.class)
+public class CustomerControllerHttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void customerShouldReturnCustomerInformation() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/customer/customer/allCustomers", String.class)).contains("[{\"id\":2,\"firstName\":\"Jens\",\"lastName\":\"Krupp\",\"emailAdress\":\"Jens.Krupp@gmail.com\"}]");
+    }
+}
